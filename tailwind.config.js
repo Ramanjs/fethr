10e058c0fe26a16a6e5cc6b978c16af4321449dd
Ramanjs <raman.s2662@gmail.com>
2023-03-17 18:24:58 +0530

    initialise project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './app/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+
+        // Or if using `src` directory:
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            Gazapacho: ['Gazapacho', 'serif'],
+        },
+        colors: {
+            main: '#2C0058',
+            secondary: '#FEF7ED',
+            accent: '#AE4911',
+            base: '#FEF7ED',
+        },
+        screens: {
+            small: {
+                max: '800px',
+            },
+        },
+        extend: {
+            animation: {
+                'spin-slow': 'spin 3s linear infinite',
+            },
+        },
+    },
+    plugins: [],
+}
